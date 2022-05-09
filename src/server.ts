7d6b7618519689fb14d0077089d90ebf8c190949
Matheus Filype <matheus.campos@gcbinvestimentos.com>
2022-05-09 10:52:07 -0300

    feat: now send image in email

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { routes } from "./routes";
 import cors from "cors";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 const app = express();
 app.use(cors());
